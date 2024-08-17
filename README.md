f4ce9be462209e67bb2cad6cd241247f0f0e2e60
WH934426 <wh934426@outlook.com>
2024-08-17 17:29:36 +0800

    feat: 引入vue/vite依赖

---
+++ README.md
@@ -0,0 +1,4 @@
+# Vue3 Template
+
+此项目是通过逐步搭建项目，去实现使用 `npm init vue@latest` 创建的 vue3 + TS 项目
+
---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+
+<template>
+  <div>
+    app
+  </div>
+</template>
+
+
+
+<style lang="scss" scoped>
+
+</style>
