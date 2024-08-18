2456ebb17289e204f94a6d282bc7f1c22fcecdcd
WH934426 <wh934426@outlook.com>
2024-08-18 11:30:46 +0800

    fix: 完善路由守卫

--- src/main.js
+++ src/main.js
@@ -1,13 +1,11 @@
 import 'normalize.css/normalize.css' // 清除默认样式
 import { createApp } from 'vue'
 import App from './App.vue'
-import router, { addDynamicRoutesToLayout, addAnyRoute } from './router'
+import router, { addAnyRoute } from './router'
 import store from './stores'
 
 const app = createApp(App)
 // 路由配置
-// 确保动态路由被添加到 Layout 的 children 数组中
-addDynamicRoutesToLayout()
 // 添加任意路由
 const finalRouter = await addAnyRoute(router)
 app.use(finalRouter)
