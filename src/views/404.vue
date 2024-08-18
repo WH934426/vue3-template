d526f135ae96928f519cc7386e3eaf8304416946
WH934426 <wh934426@outlook.com>
2024-08-18 17:49:24 +0800

    feat: 调整404页面

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,7 +1,18 @@
-<script setup></script>
+<script setup>
+import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+onMounted(() => {
+	const timer = setTimeout(() => {
+		router.push('/')
+	}, 500)
+	clearTimeout(timer)
+})
+</script>
 
 <template>
-	<div>404页面</div>
+	<el-empty :image-size="200" description="404页面走丢了" />
 </template>
 
 <style lang="scss" scoped></style>
