1f4f39236446ce7519d8d3c91c97f1536dfc8e5f
WH934426 <wh934426@outlook.com>
2024-08-20 21:40:08 +0800

    feat: 布局主页面和添加过渡动画

--- src/layout/components/AppMain/index.vue
+++ src/layout/components/AppMain/index.vue
@@ -1,7 +1,21 @@
 <script setup></script>
 
 <template>
-	<el-main>main</el-main>
+	<el-main class="app-main">
+		<router-view v-slot="{ Component, route }">
+			<transition name="fade-transform" mode="out-in">
+				<component :is="Component" :key="route.path" />
+			</transition>
+		</router-view>
+	</el-main>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.app-main {
+	/* 50= navbar  */
+	min-height: calc(100vh - 50px);
+	width: 100%;
+	overflow: hidden;
+	position: relative;
+}
+</style>
